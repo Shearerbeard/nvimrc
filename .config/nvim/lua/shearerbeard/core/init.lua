868e80ff7c57fc34c7bc950b149e8c889b58f88e
Michael Shearer <mshearer@pesa.com>
2023-11-27 16:31:44 -0700

    first working config with lsp

--- .config/nvim/lua/shearerbeard/core/init.lua
+++
@@ -1,9 +0,0 @@
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- BASE
-vim.g.mapleader = ' '
-vim.opt.termguicolors = true
-
-
-require("shearerbeard.core.keybind")
